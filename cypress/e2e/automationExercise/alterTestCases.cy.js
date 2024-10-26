1808ad109694e7dcd7a694c36862dc2d49f20287
bruno <bruno.aurelio89@hotmail.com>
2024-10-26 15:47:47 -0300

    fix-alterando execução apenas de 1 test, alterando hora para n2, alterando condição para n3

--- cypress/e2e/automationExercise/alterTestCases.cy.js
+++ cypress/e2e/automationExercise/alterTestCases.cy.js
@@ -21,51 +21,51 @@ describe("Suite Test Case of Automation Exercises", () => {
     RegisterNew.DeleteAccountCreated();
   });
 
-  it("Test Case 2: Login User with correct email and password", () => {
-    ActionButtons.ClickSignup();
-    UserLogin.CorrectUser();
-  });
+  // it("Test Case 2: Login User with correct email and password", () => {
+  //   ActionButtons.ClickSignup();
+  //   UserLogin.CorrectUser();
+  // });
 
-  it("Test Case 3: Login User with incorrect email and password", () => {
-    ActionButtons.ClickSignup();
-    UserLogin.IncorretUser();
-  });
+  // it("Test Case 3: Login User with incorrect email and password", () => {
+  //   ActionButtons.ClickSignup();
+  //   UserLogin.IncorretUser();
+  // });
 
-  it("Test Case 4: Logout User", () => {
-    ActionButtons.ClickSignup();
-    UserLogin.CorrectUser();
-    ActionButtons.ClickLogout();
-  });
+  // it("Test Case 4: Logout User", () => {
+  //   ActionButtons.ClickSignup();
+  //   UserLogin.CorrectUser();
+  //   ActionButtons.ClickLogout();
+  // });
 
-  it("Test Case 5: Register User with existing email", () => {
-    ActionButtons.ClickSignup();
-    UserLogin.SignupExistUser();
-  });
+  // it("Test Case 5: Register User with existing email", () => {
+  //   ActionButtons.ClickSignup();
+  //   UserLogin.SignupExistUser();
+  // });
 
-  it("Test Case 6: Contact Us Form", () => {
-    ActionButtons.ContactUs();
-    ContactUs.contactFillForm();
-  });
+  // it("Test Case 6: Contact Us Form", () => {
+  //   ActionButtons.ContactUs();
+  //   ContactUs.contactFillForm();
+  // });
 
-  it("Test Case 8: Verify All Products and product detail page", () => {
-    ActionButtons.ClickProducts();
-    FindAllProducts.FindAll();
-  });
+  // it("Test Case 8: Verify All Products and product detail page", () => {
+  //   ActionButtons.ClickProducts();
+  //   FindAllProducts.FindAll();
+  // });
 
-  it("Test Case 9: Search Product", () => {
-    ActionButtons.ClickProducts();
-    SearchProducts.Search();
-  });
+  // it("Test Case 9: Search Product", () => {
+  //   ActionButtons.ClickProducts();
+  //   SearchProducts.Search();
+  // });
 
-  it("Test Case 10: Verify Subscription in home page", () => {
-    Subscription.NewSubscription();
-  });
+  // it("Test Case 10: Verify Subscription in home page", () => {
+  //   Subscription.NewSubscription();
+  // });
 
-  it("Test Case 15: Place Order: Register before Checkout", () => {
-    RegisterNew.fillForm();
-    Item.AddNewItemCart();
-    CartCheckout.Checkout();
-    Payment.MethodPayment();
-    UserLogin.DeletedAccount();
-  });
+  // it("Test Case 15: Place Order: Register before Checkout", () => {
+  //   RegisterNew.fillForm();
+  //   Item.AddNewItemCart();
+  //   CartCheckout.Checkout();
+  //   Payment.MethodPayment();
+  //   UserLogin.DeletedAccount();
+  // });
 });
