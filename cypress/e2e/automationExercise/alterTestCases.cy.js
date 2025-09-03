b5e74cb3b859f375a9b47a72ef318857e0f8a4ba
brunoaurelio <bruno.aurelio89@hotmail.com>
2025-09-03 19:59:07 -0300

    feat-adicionando tags

--- cypress/e2e/automationExercise/alterTestCases.cy.js
+++ cypress/e2e/automationExercise/alterTestCases.cy.js
@@ -16,52 +16,52 @@ describe("Suite Test Case of Automation Exercises", () => {
     cy.visit("/");
   });
 
-  it("Test Case 1: Register User", () => {
+  it("Test Case 1: Register User", {tags: 'register'},  () => {
     RegisterNew.fillForm();
     RegisterNew.DeleteAccountCreated();
   });
 
-  it("Test Case 2: Login User with correct email and password", () => {
+  it("Test Case 2: Login User with correct email and password", {tags: 'login'}, () => {
     ActionButtons.ClickSignup();
     UserLogin.CorrectUser();
   });
 
-  it("Test Case 3: Login User with incorrect email and password", () => {
+  it("Test Case 3: Login User with incorrect email and password", {tags: 'login'}, () => {
     ActionButtons.ClickSignup();
     UserLogin.IncorretUser();
   });
 
-  it("Test Case 4: Logout User", () => {
+  it("Test Case 4: Logout User",  {tags: 'login'},() => {
     ActionButtons.ClickSignup();
     UserLogin.CorrectUser();
     ActionButtons.ClickLogout();
   });
 
-  it("Test Case 5: Register User with existing email", () => {
+  it("Test Case 5: Register User with existing email", {tags: 'register'},() => {
     ActionButtons.ClickSignup();
     UserLogin.SignupExistUser();
   });
 
-  it("Test Case 6: Contact Us Form", () => {
+  it("Test Case 6: Contact Us Form", {tags: 'contact'},() => {
     ActionButtons.ContactUs();
     ContactUs.contactFillForm();
   });
 
-  it("Test Case 8: Verify All Products and product detail page", () => {
+  it("Test Case 8: Verify All Products and product detail page", {tags: ['login', 'product']},() => {
     ActionButtons.ClickProducts();
     FindAllProducts.FindAll();
   });
 
-  it("Test Case 9: Search Product", () => {
+  it("Test Case 9: Search Product",{tags: 'product'}, () => {
     ActionButtons.ClickProducts();
     SearchProducts.Search();
   });
 
-  it("Test Case 10: Verify Subscription in home page", () => {
+  it("Test Case 10: Verify Subscription in home page", {tags: 'subscription'},() => {
     Subscription.NewSubscription();
   });
 
-  it("Test Case 15: Place Order: Register before Checkout", () => {
+  it("Test Case 15: Place Order: Register before Checkout", {tags: ['register','checkout']},  () => {
     RegisterNew.fillForm();
     Item.AddNewItemCart();
     CartCheckout.Checkout();
